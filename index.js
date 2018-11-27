01765695b69a443493bbb2e478f922f0f31d1c24
Andreas Pizsa <hopster@gmail.com>
2018-11-27 20:52:57 +0100

    feat: drop unpkg, use package-json
    Signed-off-by: Andreas Pizsa <hopster@gmail.com>

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const {join} = require('path')
-
 const badgen = require('badgen')
 const {send} = require('micro')
 const _get = require('lodash.get')
-
-const got = require('got')
-
-const jot = got.extend({
-  baseUrl: 'https://unpkg.com/',
-  json: true,
-  throwHttpErrors: false
-})
 
 module.exports = async (req, res) => {
   try {
@@ -18,23 +8,32 @@ module.exports = async (req, res) => {
       send(res, 404, '404')
       return
     }
+
     const url = require('url').parse(req.url, true)
+    const packageName = req.url.replace(/\?.*/, '').substr(1)
+    console.log(packageName)
 
-    const pkgResponse = await jot(join(req.url.replace(/\?.*/, ''), 'package.json'))
+    const pkgResponse = await (
+      require('package-json')(packageName, {fullMetadata: true})
+        .catch(() => undefined)
+    )
 
-    const hasPostinstaller = Boolean(_get(pkgResponse, 'body.postinstaller'))
-    const error = pkgResponse.statusCode >= 400
+    const error = !pkgResponse
+    const hasPostinstaller = !error && Boolean(pkgResponse.postinstaller)
     const status = error ? 'error' : hasPostinstaller ? 'ready' : 'missing'
+
     res.setHeader('Content-type', 'image/svg+xml')
     res.setHeader('X-Status', status)
-    send(res, pkgResponse.statusCode, badgen({
+    send(res, error ? 404 : 200, badgen({
       subject: 'postinstaller',
       status,
       color: error ? 'red' : hasPostinstaller ? 'green' : 'orange',
       style: url.query.style
     }))
+    res.end()
   } catch (error) {
     console.log(error)
     send(res, error.statusCode || 500, error.message || 'Error')
+    res.end()
   }
 }
